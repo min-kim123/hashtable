6ec8da723f410b7a5986dffc04b59cc84493bcc7
U-BSD\489493 <489493@SHS-DGDQCS2.beaverton.k12.or.us>
2023-02-06 12:20:48 -0800

    generates same name

--- main2.cpp
+++ main2.cpp
@@ -1,35 +1,52 @@
-#include <iostream>
+#include<iostream>
+#include<fstream>
+#include<cstdlib>
+#include<cctype>
+#include <cstring>
 #include <string.h>
-#include <fstream>//work with files
 using namespace std;
 // /https://www.verywellfamily.com/top-1000-baby-boy-names-2757618
 //https://namecensus.com/last-names/
 
-int main()
-{
-	string line;
-	int random = 0;
-	int numLines = 0;
-	ifstream File("lastnames.txt");
-	ifstream File2("newlastnames.txt");//rename to firstnames.txt
-	char first[20];
-	char last[20];
+void generate(int num) {
+  char line[80];
+  int i = 0;
+  int random = 0;
+  int numLines = 0;
+  ifstream File("lastnames.txt");
+  ifstream File2("firstnames.txt");//rename to firstnames.txt
+  char first[20];
+  char last[20];
 
-	srand(time(0));
-	random = rand() % 500;
-	while (getline(File, line)) {
-		++numLines;
-		if (numLines == random) {
-			strcpy(last, line.c_str());
-		}
-	}
-	numLines = 0;
-	random = rand() % 1000;
-	while(getline(File2, line)) {
-		++numLines;
-		if (numLines == random) {
-			strcpy(first, line.c_str());
-		}
-	}
-	cout << first << " " << last << endl;
+  while (i != num) {
+    i++;
+    srand(time(0));
+    random = rand() % 500;
+
+    while (File.getline(line, sizeof(line))) {
+      ++numLines;
+      if (numLines == random) {
+	strcpy(last, line);
+
+      }
+    }
+    numLines = 0;
+    random = rand() % 1000;
+    while(File2.getline(line, sizeof(line))) {
+      ++numLines;
+      if (numLines == random) {
+	strcpy(first, line);
+
+      }
+    }
+    cout << first << " " << last << endl;
+  }
+	
+}
+int main() {
+  int num = 0;
+  cout << "How many students? " ;
+  cin >> num;
+  generate(num);
+
 }
