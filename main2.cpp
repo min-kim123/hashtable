f35110b0d7bd03882a938ae5124130772a22143c
Min Kim <8450minkim@gmail.com>
2023-02-19 23:29:33 -0800

    workign on delete

--- main2.cpp
+++ main2.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <fstream>
-#include <cstdlib>
 #include <cctype>
 #include <cstring>
 #include <iomanip>
@@ -9,15 +8,15 @@
 #include "node.h"
 using namespace std;
 
-void add(int &newID, int amount, Node* hashtable[], int &arraySize, int &totalNodes);
+void add(int &newID, int amount, Node** &hashtable, int &arraySize);
 int hashfunction(Node* n, int arraySize);
 void getLine(fstream& file, int num, char* line);
-void print(Node* hashtable[], int arraySize);
-void deleet(int arraySize, Node* hashtable[], int &totalNodes);
-//void rehash(int arraySize, Node* hashtable[]);
+void print(Node** hashtable, int arraySize);
+void deleet(int arraySize, Node** hashtable);
+void rehash(int &arraySize, Node** &hashtable);
+void add_manual(int &newID, int &arraySize, Node** &hashtable);
 
 int main() {
-  int totalNodes = 0;//total number of students, add to when student added, delete when student deleted
   int newID= 0;//ID number, incremented by 1 for each new student
   int amount = 0;
   bool cont = true;
@@ -33,18 +32,49 @@ int main() {
     cout << "add or print?: "; 
     cin >> input;
     cin.ignore();
-    if (strcmp(input, "add") == 0) {
+    if (strcmp(input, "generate") == 0) {
       cout << "How many students? " ;
       cin >> amount;
-      add(newID, amount, hashtable, arraySize, totalNodes);
+      add(newID, amount, hashtable, arraySize);
       //getLine, hashfunction called from add
+    }
+    else if (strcmp(input, "add")==0) {
+      add_manual(newID, arraySize, hashtable);
     }
     else if (strcmp(input, "print")==0) {
       print(hashtable, arraySize);
     }
+
+
     else if (strcmp(input, "delete")==0) {
-      deleet(arraySize, hashtable, totalNodes);
+      int id;
+      cout << "ID: ";
+      cin >> id;
+      for (int i = 0; i < arraySize; ++i) {
+        if (head == NULL) {
+          cout << "Empty list. Nothing to delete" << endl;
+        }
+        else if (id == head->getStudent()->getID()) {//when the first one is being deleted, special case since head is being changed.
+            Node* temp = head;
+            if (head->getNext() == NULL) {//there is only one node
+                head = NULL;
+            }
+            else {
+                cout << "else  changing head" << endl;
+                head = head->getNext();
+                delete temp;
+            }
+        }
+        else {
+            deleet(head, id);
+        }
+      }
+      
+      deleet(arraySize, hashtable);
     }
+
+
+
     else if (strcmp(input, "quit")==0) {
       cont = false;
     }
@@ -52,36 +82,23 @@ int main() {
       cout << "Invalid input" << endl;
     }
   }
-  delete[] hashtable;
 }
 
-void print(Node* hashtable[], int arraySize) {
+void print(Node** hashtable, int arraySize) {
   int total = 0;
   for (int i = 0; i < arraySize; ++i ){
     Node* n = hashtable[i];
     cout << i << ": ";
     while (n != NULL) {
-      cout << "|" << n->getStudent()->getFirst() << " " << n->getStudent()->getLast() << " " << setprecision(3) <<n->getStudent()->getGPA() << " " << n->getStudent()->getID() << "|";
+      cout << "|" << n->getStudent()->getFirst() << " " << n->getStudent()->getLast()  << " " << n->getStudent()->getID()  << " ";
+      cout << fixed << setprecision(2);
+      cout << n->getStudent()->getGPA()<<"|";
       ++total;
       n = n->getNext();
     }
     cout << endl;
   }
   cout << "total nodes printed: " << total << endl;
-}
-
-int hashfunction(Node* n, int arraySize) {//how to get the key
-  char last[80];
-  strcpy(last, n->getStudent()->getLast());//copy the lastname char array into last
-  //add all ascii values of last name and mod
-  int i = 0;
-  int total = 0;
-  while (last[i] != '\0') {
-    total = total+(int)last[i];
-    ++i;
-  }
-  int key = total%arraySize;
-  return key;
 }
 
 void getLine(fstream& file, int num, char* line){//had to put this stuff outside the generate function because instead of starting from line 1 in started from the previous line
@@ -94,7 +111,47 @@ void getLine(fstream& file, int num, char* line){//had to put this stuff outside
     strcpy(line, fileline);
 }
 
-void add(int &newID, int amount, Node* hashtable[], int &arraySize, int &totalNodes) {//make a random student, increment id by 1 each time
+void add_manual(int &newID, int &arraySize, Node** &hashtable) {
+  int count = 1;
+  ++newID;
+  char newfirst[20];
+  char newlast[20];
+  float newGPA;
+  cout << "First Name: ";
+  cin >> newfirst;
+  cin.ignore();
+  cout << "Last Name: ";
+  cin >> newlast;
+  cin.ignore();
+  cout << "ID: " << newID<<endl;
+  cout << "GPA: " ;
+  cin >> newGPA;   
+
+  Student* newstudent = new Student(newfirst, newlast, newID, newGPA);
+  Node* newnode = new Node(newstudent);//put newstudent into new node
+  int key = hashfunction(newnode, arraySize);//make the key for the newnode
+
+  Node* n = hashtable[key];
+  if (n == NULL) {//newnode is first one with that key
+    hashtable[key] = newnode;
+  }
+  else {
+    while (n!= NULL) {
+      ++count;
+      if (n->getNext() == NULL) {
+        if (count == 3) {
+          cout << "-------------rehash" << endl;
+          rehash(arraySize, hashtable);
+          break;
+        }
+        n->setNext(newnode);
+        break;
+      }
+      n = n->getNext();
+    }
+  }
+}
+void add(int &newID, int amount, Node** &hashtable, int &arraySize) {//make a random student, increment id by 1 each time
   int random = 0;
   int i = 0;
   fstream File("lastnames.txt");
@@ -121,88 +178,124 @@ void add(int &newID, int amount, Node* hashtable[], int &arraySize, int &totalNo
 
     //add it into the table
     int count = 1;//to count number of nodes in linked list
-    int slotsTaken = 0;
     Node* n = hashtable[key];
     if (n == NULL) {//newnode is first one with that key
       hashtable[key] = newnode;
-      ++slotsTaken;//if this reaches half the array size, rehash
     }
     else {
       cout << "NOT NULL" << endl;
       while (n!= NULL) {
         ++count;
         if (n->getNext() == NULL) {
+          if (count == 3) {
+            cout << "-------------rehash" << endl;
+            rehash(arraySize, hashtable);
+            break;
+          }
           n->setNext(newnode);
           break;
         }
         n = n->getNext();
-        if (count == 2) {
-          cout << "-------------rehash" << endl;
-         //rehash(arraySize, hashtable);
-        }
       }
     }
-    ++totalNodes;//add 1 student
     cout << newfirst << " " << newlast << " " << newGPA << " " << newID << " " << key << endl;
   }
 }
-/*
-void rehash(int arraySize, Node* hashtable[]) {
-  cout << "-----------REHASH-----------" << endl;
+
+void rehash(int &arraySize, Node** &hashtable) {
+  cout << "1. in rehash" << endl;
   arraySize = arraySize*2;
-  Node* newtable[arraySize];
-  for (int i = 0; i < arraySize; ++i) {
+  Node** newtable = new Node*[arraySize];
+  for (int i = 0; i < arraySize; ++i) {//set all slots in newtable to null
     newtable[i] = NULL;
   }
-  for (int i = 0; i < arraySize/2; ++i) {//copy hashtable into temp
-    Node* nHashtable = hashtable[i];
-    while (nHashtable != NULL) {
-      for (int i = 0; i < arraySize; ++i) {
-        if (i == hashfunction(nHashtable, arraySize)) {
-          Node* nNewtable = newtable[i];
-          if(nNewtable == NULL) {//it is the first one
-            nNewtable = nHashtable;
+  //node does not get added to the newtable if it is not the first node in the array slot of hashtable
+  for (int i = 0; i < arraySize/2; ++i) {//for every slot in hashtable array
+    //cout << "2. in for lop" << endl;
+    Node* nHashtable = hashtable[i];//nHashtable is the ith sloth in hashtable
+    while (nHashtable != NULL) {//while that node in the ith slot is not null
+      int key = hashfunction(nHashtable, arraySize);
+      Node* nNewtable = newtable[key];//node pointer nNewtable is the first node at the key slot in newtable
+      //cout << "3. in while loop" << endl;
+      if (nNewtable == NULL) {//it is the first one
+        //cout << "5" << endl;
+        newtable[key] = nHashtable; //put the node from hashtable into newtable
+        newtable[key]->setNext(NULL);
+      }
+      else{//not the first 
+        //cout << "6" << endl;
+        while (nNewtable != NULL) {
+          //cout << "7" << endl;
+          if (nNewtable->getNext() == NULL) {
+            //cout << "8" << endl;
+            nNewtable->setNext(nHashtable);
+            nNewtable->getNext()->setNext(NULL);
+            break;
           }
-          else{//not the first 
-            if (nNewtable->getNext() == NULL) {
-              nNewtable->setNext(nHashtable);
-              nNewtable = nHashtable;
-              //break;
-            }
-            nNewtable = nNewtable->getNext();
-          }
+          nNewtable = nNewtable->getNext();
         }
       }
       nHashtable = nHashtable->getNext();
     }
   }
-  hashtable = &newtable[0];
-
-  //print(hashtable, arraySize);
-  for (int i = 0; i < arraySize; ++i ){
-    Node* n = hashtable[i];
-    cout << i << ": ";
-    while (n != NULL) {
-      cout << "|" << n->getStudent()->getFirst() << " " << n->getStudent()->getLast() << " " << setprecision(3) <<n->getStudent()->getGPA() << " " << n->getStudent()->getID() << "|";
-      n = n->getNext();
-    }
-    cout << endl;
-  }
 }
-*/
-void deleet (int arraySize, Node* hashtable[], int &totalNodes) {
-  --totalNodes;//subtract 1
+
+void deleet (int arraySize, Node** hashtable) {
   int input;
   cout << "ID: ";
   cin >> input;
   for (int i = 0; i < arraySize; ++i) {
+    cout << i << endl;
     Node* n = hashtable[i];
-    while (hashtable[i]!= NULL) {
-      if (n->getStudent()->getID() == input) {
-
+    if (n != NULL) {
+      if (n->getStudent()->getID() == input) {//head is the id
+        cout << "2" << endl;
+        Node* temp = n;
+        if (n->getNext() == NULL) {
+          cout << "3" << endl;
+          Node* temp = n;
+          n = NULL;
+          delete temp;
+        }
+      else {
+        hashtable[i] = n->getNext();
+        delete temp;
         break;
+      }
+      
+      }
+    }
+    
+    while (n != NULL) {
+      if (n->getNext()->getStudent()->getID() == input) {
+        Node* temp = n->getNext();
+        if (n->getNext()->getNext() == NULL) {
+          n->setNext(NULL);
+          delete temp;
+          break;
+        }
+        else {
+          n->setNext(n->getNext()->getNext());
+          delete temp;
+          break;
+        }
+
       }
       n = n->getNext();
     }
   }
+}
+
+int hashfunction(Node* n, int arraySize) {//how to get the key
+  char last[80];
+  strcpy(last, n->getStudent()->getLast());//copy the lastname char array into last
+  //add all ascii values of last name and mod
+  int i = 0;
+  int total = 0;
+  while (last[i] != '\0') {
+    total = total+(int)last[i];
+    ++i;
+  }
+  int key = total%arraySize;
+  return key;
 }
